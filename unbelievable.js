7aa2204c5c9283731fa2d9c0e19ff4f8c516e633
chensisihai <chensi0424@163.com>
2018-08-23 17:14:36 +0800

    Create unbelievable.js

---
+++ unbelievable.js
@@ -0,0 +1,15 @@
+1、 var arr = [0, 1, 2];
+    arr[10] = 6;
+    arr.filter(funtion(x) {
+        return x === undefined;
+    });
+    
+    结果为： []
+    
+    原因： filter执行的时候首先检查索引值是不是数组的一个属性  0 in Array => true;  5 in Array => false; 10 in Array => true  ，也就是说 3-9都是
+    没有初始化的，是不存在的， array的函数调用时会跳过这些坑
+      
+    console.log(arr.length) // 11
+    console.log(arr[5]) // undefined
+    
+    
