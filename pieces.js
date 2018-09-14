e8099fcb65d2333a38494a2692d0f713285f6252
chensisihai <chensi0424@163.com>
2018-09-14 16:05:38 +0800

    Update pieces.js

--- pieces.js
+++ pieces.js
@@ -30,3 +30,41 @@
     var foo = "11"+2+"1";    //体会加一个字符串'1' 和 减去一个字符串'1'的不同
     console.log(foo); // '1121'
     console.log(typeof foo); // String
+
+4、// 为了保证页面输出安全，我们经常需要对一些特殊的字符进行转义，请写一个函数escapeHtml，将<, >, &, “进行转义
+
+    function escapeHtml(str) {
+        return str.replace(/[<>”&]/g, function(match) {
+            switch (match) {
+                   case “<”:
+                      return “<”;
+                   case “>”:
+                      return “>”;
+                   case “&”:
+                      return “&”;
+                   case “\””:
+                      return “"”;
+              }
+          });
+    }
+
+5、 foo = foo ||bar // 这行代码是什么意思？为什么要这样写？
+    //  等价于
+    if(!foo) foo = bar; //如果foo存在，值不变，否则把bar的值赋给foo。
+
+    // 短路表达式：作为"&&"和"||"操作符的操作数表达式，这些表达式在进行求值时，只要最终的结果已经可以确定是真或假，求值过程便告终止，这称之为短路求值。
+
+
+6、for(var i=1;i<=3;i++){
+      setTimeout(function(){
+          console.log(i);   
+      },0); 
+    };  // 4, 4, 4
+
+ //  如何让上述代码输出 1，2， 3
+
+   for(var i=1;i<=3;i++){
+       setTimeout((function(a){  //改成立即执行函数
+           console.log(a);   
+       })(i),0); 
+   };
