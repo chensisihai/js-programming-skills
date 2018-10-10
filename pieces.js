a7339f435101ddd9170364116587d0b959293663
chensisihai <chensi0424@163.com>
2018-10-10 15:37:58 +0800

    Update pieces.js

--- pieces.js
+++ pieces.js
@@ -149,3 +149,32 @@
 
         // （1） 作为函数的参数，表示该函数的参数不是对象。
         // （2） 作为对象原型链的终点。
+
+11、 // 创建ajax的过程
+    
+   // (1)创建`XMLHttpRequest`对象,也就是创建一个异步调用对象.
+
+   // (2)创建一个新的`HTTP`请求,并指定该`HTTP`请求的方法、`URL`及验证信息.
+
+  //  (3)设置响应`HTTP`请求状态变化的函数.
+
+   // (4)发送`HTTP`请求.
+
+   // (5)获取异步调用返回的数据.
+
+   // (6)使用JavaScript和DOM实现局部刷新.
+
+
+    var xmlHttp = new XMLHttpRequest();
+
+    xmlHttp.open('GET','demo.php','true');
+
+    xmlHttp.send()
+
+    xmlHttp.onreadystatechange = function(){
+
+        if(xmlHttp.readyState === 4 & xmlHttp.status === 200){
+
+        }
+
+    }
